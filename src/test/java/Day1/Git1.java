40de8f548c05160b3e722dab434f1bc312478693
Mustafa S <101791673+89045@users.noreply.github.com>
2022-03-18 17:34:00 +0300

    Update Git1.java

--- src/test/java/Day1/Git1.java
+++ src/test/java/Day1/Git1.java
@@ -4,6 +4,8 @@ public class Git1 {
   public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("2.satir calisması");
+    
+        System.out.println("3.satir calisması");
 
 
     }
