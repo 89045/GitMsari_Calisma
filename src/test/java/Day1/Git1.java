407838b8395f5e9a47128554713c17c95cae1849
89045 <msari5734@gmail.com>
2022-03-22 12:17:11 +0300

    My test file uploded

--- src/test/java/Day1/Git1.java
+++ src/test/java/Day1/Git1.java
@@ -5,6 +5,7 @@ public class Git1 {
   public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("2.satir calisması");
+
     
         System.out.println("3.satir calisması");
 
